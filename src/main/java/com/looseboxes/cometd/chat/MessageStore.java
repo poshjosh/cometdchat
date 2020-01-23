4e9c501d9b0c122cc112fc9835bac81938641b04
poshjosh <posh.bc@gmail.com>
2020-01-23 17:54:21 +0100

    Rename class/interface by removing Private from previous name. This class/interface is agnostic to whether the message is private or not.

---
+++ src/main/java/com/looseboxes/cometd/chat/MessageStore.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2018 NUROX Ltd.
+ *
+ * Licensed under the NUROX Ltd Software License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.looseboxes.com/legal/licenses/software.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.looseboxes.cometd.chat;
+
+import java.util.List;
+
+/**
+ * @author Chinomso Bassey Ikwuagwu on May 18, 2018 7:46:06 PM
+ */
+public interface MessageStore<M> {
+    
+    List<M> get(String from, String to);
+}
